35b2f613b231385c6d598bdbbfc7690ac0b57d28
Lucy Fox <lucyreneefox@gmail.com>
2016-08-08 18:33:01 -0600

    Add Jade passing challenge

--- jade/home.js
+++ jade/home.js
@@ -2,14 +2,10 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
-// app.use(express.static(process.argv[3] || path.join(__dirname, 'public')))
-
-// app.listen(process.argv[2]);
-
-app.set('views', path.join(__dirname, 'templates'))
 app.set('view engine', 'jade')
+app.set('views', path.join(__dirname, 'templates'))
 
-app.get('/home', function(req, res){
+app.get('*', function(req, res){
   res.render('index', {date: new Date().toDateString()})
 });
 
